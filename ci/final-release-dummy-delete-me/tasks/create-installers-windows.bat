559df702290296b82a9837e1615b85b99e0073bc
Jeff Pak <jeffrey.pak@emc.com>
2016-07-28 16:38:56 -0700

    change to cli-ci

--- ci/final-release-dummy-delete-me/tasks/create-installers-windows.bat
+++ ci/final-release-dummy-delete-me/tasks/create-installers-windows.bat
@@ -9,17 +9,17 @@ REM Name: "signtool", Command: "signtool.exe $p"
 REM This is to add "signtool.exe" to your path, so it does not need to be fully qualified in the configuration above
 SET PATH=C:\Program Files (x86)\Windows Kits\10\bin\x64;%PATH%
 
-sed -i -e "s/VERSION/6.19.0/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/SIGNTOOL_CERT_PASSWORD/%SIGNTOOL_CERT_PASSWORD%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/SIGNTOOL_CERT_PATH/%SIGNTOOL_CERT_PATH%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
+sed -i -e "s/VERSION/6.19.0/" %ROOT_DIR%\cli-ci\installers\windows\windows-installer-x64.iss
+sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli-ci\installers\windows\windows-installer-x64.iss
+sed -i -e "s/SIGNTOOL_CERT_PASSWORD/%SIGNTOOL_CERT_PASSWORD%/" %ROOT_DIR%\cli-ci\installers\windows\windows-installer-x64.iss
+sed -i -e "s/SIGNTOOL_CERT_PATH/%SIGNTOOL_CERT_PATH%/" %ROOT_DIR%\cli-ci\installers\windows\windows-installer-x64.iss
 
 pushd %ROOT_DIR%\64bit-windows-binary
 	MOVE cf-cli_6.19.0_winx64 ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
+ISCC %ROOT_DIR%\cli-ci\installers\windows\windows-installer-x64.iss
 
-MOVE %ROOT_DIR%\cli\installers\windows\Output\mysetup.exe cf_installer.exe
+MOVE %ROOT_DIR%\cli-ci\installers\windows\Output\mysetup.exe cf_installer.exe
 
 zip %ROOT_DIR%\winstallers\cf-cli-installer_winx64.zip cf_installer.exe
