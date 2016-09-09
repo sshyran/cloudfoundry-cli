9fd26b05c89dbcae4a5b51a9422c3195075ed74c
Jeff Pak <jeffrey.pak@emc.com>
2016-09-09 15:05:56 -0700

    WIP

--- commands/v2/unbind_service_command.go
+++ commands/v2/unbind_service_command.go
@@ -1,24 +1,35 @@
 package v2
 
 import (
-	"os"
-
-	"code.cloudfoundry.org/cli/cf/cmd"
 	"code.cloudfoundry.org/cli/commands"
 	"code.cloudfoundry.org/cli/commands/flags"
 )
+
+//go:generate counterfeiter . UnbindServiceActor
+
+type UnbindServiceActor interface {
+}
 
 type UnbindServiceCommand struct {
 	RequiredArgs    flags.BindServiceArgs `positional-args:"yes"`
 	usage           interface{}           `usage:"CF_NAME unbind-service APP_NAME SERVICE_INSTANCE"`
 	relatedCommands interface{}           `related_commands:"apps, delete-service, services"`
+
+	UI     commands.UI
+	Actor  UnbindServiceActor
+	Config commands.Config
 }
 
-func (_ UnbindServiceCommand) Setup(config commands.Config, ui commands.UI) error {
+func (cmd UnbindServiceCommand) Setup(config commands.Config, ui commands.UI) error {
+	cmd.UI = ui
+	cmd.Config = config
 	return nil
 }
 
 func (_ UnbindServiceCommand) Execute(args []string) error {
-	cmd.Main(os.Getenv("CF_TRACE"), os.Args)
-	return nil
+	var err error
+	if requiredArgs.AppName != nil && requiredArgs.ServiceInstance != nil {
+
+	}
+
 }
---
+++ commands/v2/unbind_service_command_test.go
@@ -0,0 +1,82 @@
+package v2_test
+
+import (
+	"code.cloudfoundry.org/cli/commands/commandsfakes"
+	"code.cloudfoundry.org/cli/commands/ui"
+	. "code.cloudfoundry.org/cli/commands/v2"
+	"code.cloudfoundry.org/cli/commands/v2/v2fakes"
+	"code.cloudfoundry.org/cli/utils/config"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("Unbind Service Command", func() {
+	var (
+		cmd        UnbindServiceCommand
+		fakeUI     ui.UI
+		fakeActor  *v2fakes.FakeUnbindServiceActor
+		fakeConfig *commandsfakes.FakeConfig
+	)
+
+	BeforeEach(func() {
+		out := NewBuffer()
+		fakeUI = ui.NewTestUI(out, out)
+		fakeActor = new(v2fakes.FakeUnbindServiceActor)
+		fakeConfig = new(commandsfakes.FakeConfig)
+
+		cmd = UnbindServiceCommand{
+			UI:    fakeUI,
+			Actor: fakeActor,
+		}
+		fakeConfig.TargetedOrganizationReturns(config.Organization{
+			Name: "some-org",
+		})
+		fakeConfig.TargetedSpaceReturns(config.Space{
+			Name: "some-space",
+		})
+		fakeConfig.CurrentUserReturns(config.User{
+			Name: "admin",
+		}, nil)
+	})
+
+	Context("when both the app and service exist", func() {
+		var err error
+
+		BeforeEach(func() {
+
+		})
+
+		JustBeforeEach(func() {
+			err = cmd.Execute([]string{})
+		})
+
+		Context("when a binding exists between the app and the service", func() {
+			It("successfully unbinds the app", func() {
+			})
+		})
+
+		Context("when no binding exists between the app and the service", func() {
+			FIt("states the binding does not exist", func() {
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(fakeUI.Out).To(Say("Unbinding app %s from service %s in org %s / space %s as %s",
+					"some-app",
+					"some-service",
+					"some-org",
+					"some-space",
+					"some-user",
+				))
+				Expect(fakeUI.Out).To(Say("OK"))
+				Expect(fakeUI.Out).To(Say("Binding between %s and %s did not exist", "some-app", "some-service"))
+			})
+		})
+	})
+
+	Context("when the service does not exist", func() {
+	})
+
+	Context("when the app does not exist", func() {
+	})
+})
